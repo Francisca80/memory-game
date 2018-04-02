410e05fcc003da2125cb0f7b50592bbf6b6380ff
Francisca van maurik <franciscavanmaurik@gmail.com>
2018-04-02 09:12:25 +0200

    Added the cardopening section code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,35 @@
 /*
  * Create a list that holds all of your cards
  */
-let cardArray= [" fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor","fa fa-bolt","fa fa-cube","fa fa-leaf","fa fa-anchor","fa fa-bicycle","fa fa-diamond","fa fa-bomb","fa fa-leaf","fa fa-bomb","fa fa-bolt",
-"fa fa-bicycle","fa fa-paper-plane-o","fa fa-cube"];
+const cardArray= [
+    "fa-diamond",
+    "fa fa-paper-plane-o",
+    "fa fa-anchor",
+    "fa fa-bolt",
+    "fa fa-cube",
+    "fa fa-leaf",
+    "fa fa-anchor",
+    "fa fa-bicycle",
+    "fa fa-diamond",
+    "fa fa-bomb",
+    "fa fa-leaf",
+    "fa fa-bomb",
+    "fa fa-bolt",
+    "fa fa-bicycle",
+    "fa fa-paper-plane-o",
+    "fa fa-cube"
+  ];
 
+/* een refentie naar de open en matched en shuffled kaarten.
+*/
+
+let openCards: [];
+let matchedCards [];
+let shuffledCards [];
+
+/* refeentie naar alle ids
+*/
+const deck = create.getElementById('deck');
 
 /*
  * Display the cards on the page
@@ -12,9 +38,27 @@ let cardArray= [" fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor","fa fa-b
  *   - add each card's HTML to the page
  */
 
+function initGame();
+ shuffledCards = shuffle(cardArray);
+ createDeck(shuffledCards);
+}
+
+function createDeck(shuffledCards){
+  for(var i = 0;i ,shuffledCards.lenght;i++){
+    let li = document.createElement('li');
+    let i = document.createElement('i');
+    i.classList.add('fa');
+    i.classList.add(shuffledCards[i]);
+    li.appendChild(i);
+    deck.appendChild(li);
+
+    li.addEventListener(click.turnCard);
+  }
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
+
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
@@ -26,16 +70,15 @@ function shuffle(array) {
     }
 
     return array;
-}
 
-function startGame(){
-for (var i =0; i < cardArray.length; i++){
- i.className.remove("show");
-shuffle(cardArray);
-}
-}
+function turnCard(event){}
 
 
+  initGame();  
+/*Write a code which will add cards on page - you have exemplary code in HTML file,
+ how this can looks like... You can use for this for example for loop and
+  innerHTML method to add elements <li> for example to element <ul> in HTML file,
+in which you will have your cards ... For example in <div> or <span> elements...
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
