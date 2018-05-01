1de3d3f703fff4a636bd823ba581ab2053a4e360
Francisca van Maurik <francisca@MacBook-Pro-van-Maki.local>
2018-05-01 16:58:50 +0200

    added pokemonicons and changed matched function

--- js/app.js
+++ js/app.js
@@ -1,36 +1,64 @@
 /*
  * Create a list that holds all of your cards
  */
-const cards = [
-    "fa-diamond",
-    "fa-paper-plane-o",
-    "fa-anchor",
-    "fa-bolt",
-    "fa-cube",
-    "fa-leaf",
-    "fa-anchor",
-    "fa-bicycle",
-    "fa-diamond",
-    "fa-bomb",
-    "fa-leaf",
-    "fa-bomb",
-    "fa-bolt",
-    "fa-bicycle",
-    "fa-paper-plane-o",
-    "fa-cube"
-];
+const cardsAll = [
+    {
+        id: 1,
+        name: 'mew',
+        img: "img/001-mew.png"
+    },
+    {
+        id: 2,
+        name: 'pokeball',
+        img:  "img/002-pokeball.png"
+    },
+
+    {   id:3,
+        name:'charmander',
+        img: "img/003-charmander.png"
+    },
+
+    {   id:4,
+        name: 'bullbasaur',
+        img: "img/004-bullbasaur.png"
+    },
+    {   id:5,
+        name:'squirtle',
+        img: "img/005-squirtle.png"
+    },
+    {   id:6,
+        name:'eevee',
+        img: "img/006-eevee.png"
+    },
+    {   id:7,
+        name:'snorlax',
+        img:"img/007-snorlax.png"
+    },
+    {   id:8,
+        name: 'pikachu',
+        img:"img/008-pikachu.png"
+    }
+]; 
+
+//create pairs
+
+const cards = cardsAll
+    .concat(cardsAll);
+
 
 /* a refence to the matched and shuffledCards.
  */
 
 let openCards = [];
-let matchedCards = [];
-let shuffledCards = [];
+let matchedCards = document.getElementsByClassName("card open show matched");
+
 
 let moves = 0;
 let counter = document.querySelector(".moves");
 
-
+let pairs= [];
+let matches = 0;
+let pairedCards = document.getElementsByClassName("card open show match");
 const stars = document.querySelectorAll(".fa-star");
 
 let modal = document.querySelector(".modal");
@@ -38,7 +66,9 @@ let closeicon = document.querySelector(".close");
 
 let pAgain = document.querySelector(".btn.playagain");
 
-const deck = document.getElementById("deck");
+const deck = document.querySelector(".deck");//deck element with all the acrads
+const card = document.querySelectorAll(".card"); //card element
+
 
 /*
  * Display the cards on the page
@@ -47,31 +77,90 @@ const deck = document.getElementById("deck");
  *   - add each card's HTML to the page
  */
 
-function initGame() {
 
-    /* shuffle the cards and assign them to shuffledCards
-     */
-    shuffledCards = shuffle(cards);
+// Shuffle function from http://stackoverflow.com/a/2450976
 
-    createDeck(shuffledCards);
+function shuffle(array) {
+
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    createDeck();
+    return array;
 }
+shuffle(cards);
 
 /* function to create the deck
  */
-function createDeck(shuffledCards) {
+function createDeck() {
 
-    for (var i = 0; i < shuffledCards.length; i++) {
-        let liEl = document.createElement("li");
-        liEl.classList.add("card");
-        let iEl = document.createElement("i");
-        iEl.classList.add("fa");
-        iEl.classList.add(shuffledCards[i]);
-        liEl.appendChild(iEl);
-        deck.appendChild(liEl);
-        liEl.addEventListener("click", turnCard);
+    for (let i = 0; i < card.length; i++) {
+        card[i].children.item(0).src = cards[i].img;
+        card[i].dataset.id =cards[i].id;
     }
 }
 
+function turnCard(event) {
+    if (!event.target.classList.contains('card')) return; // If the target isn't a card, stop the function
+
+    if (openCards.length < 2) {
+        event.target.classList.add("open", "show");
+        openCards.push(event.target);
+    }
+    if (openCards.length == 2) {
+        deck.removeEventListener('click', turnCard); // to stop from further clicking on cards until animation is finished
+        checkMatch(openCards[0], openCards[1]);
+    }
+
+}
+    /* add the class show to event.target.*/
+  //  event.target.classList.add("show");
+
+    /*remove the eventlistener to avoid doubleclik and fill the opencards array
+    with to times te same card,when no match was fount the listener gets added again.*/
+  //  event.target.removeEventListener("click", turnCard);
+
+  //  checkCards(event);
+
+//}
+/* *** *** MATCH & UNMATCH COMPARISON *** *** */
+
+function checkMatch(a, b) {
+    setTimeout(function () {
+        if (a.dataset.id == b.dataset.id) {
+            //* if the cards match *//
+            openCards[0].classList.add("matched");
+            openCards[1].classList.add("matched");
+            pairs.push(openCards[0]);
+            pairs.push(openCards[1]);
+            countPairs();
+            if (++matches == 8) {
+                setTimeout(winGame(), 500);
+            }
+        } else {
+            openCards[0].classList.remove("open", "show");
+            openCards[1].classList.remove("open", "show");
+        }
+        openCards = [];
+        deck.addEventListener('click', turnCard)
+    }, 750);
+    moveCounter();
+}
+/* *** *** REMOVE MATCH*** *** */
+//removes all classes with open show match
+function removeMatch() {
+    for (i = 0; i < cards.length; i++) {
+        card[i].classList.remove("open", "show", "matched");
+    }
+    matches = 0;
+}
 // reset moves
 moves = 0;
 counter.innerHTML = moves;
@@ -90,73 +179,7 @@ var timer = document.querySelector(".timer");
 timer.innerHTML = "0 mins 0 secs";
 clearInterval(interval);
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
 
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-/* function to turn the cards,event target is li.card
-    event is the clicked card.
-*/
-
-function turnCard(event) {
-
-    /* add the class show to event.target.*/
-    event.target.classList.add("show");
-
-    /*remove the eventlistener to avoid doubleclik and fill the opencards array
-    with to times te same card,when no match was fount the listener gets added again.*/
-    event.target.removeEventListener("click", turnCard);
-
-    checkCards(event);
-
-}
-/* function to check the cards and add them to the matched cards array of put the eventlistener back*/
-function checkCards(event) {
-    /*add the cards to the opencards array*/
-
-    openCards.push(event.target.firstChild);
-
-    console.log(openCards);
-    /*Check opencards array*/
-    if (openCards.length === 2) {
-        moveCounter();
-
-        if (openCards[0].className === openCards[1].className) {
-            openCards[0].parentNode.classList.add("matched");
-            openCards[1].parentNode.classList.add("matched");
-
-
-            matchedCards.push(openCards[0]);
-            matchedCards.push(openCards[1]);
-
-        }
-        if (matchedCards === 16) {
-            winGame();
-        }
-        openCards = [];
-    } else {
-
-        setTimeout(function() {
-            for (let i = 0; i < openCards.length; i++) {
-                openCards[i].parentNode.addEventListener("click", turnCard);
-            }
-        }, 1000);
-        setTimeout(removeOpenClass, 1000);
-    }
-}
 
 
 /* reset moves*/
@@ -209,6 +232,14 @@ function startTimer() {
     }, 1000);
 }
 
+/* *** *** PAIRS COUNT *** *** */
+const pairsNo = document.querySelector(".pairs"); // span elemet which displays number of pairs
+
+function countPairs() {
+    //update pairs
+    let pairsCount = pairs.length / 2;
+    pairsNo.innerText = pairsCount;
+}
 
 function winGame() {
     clearInterval(interval);
@@ -243,10 +274,7 @@ function playAgain() {
 
 }
 
-function removeOpenClass() {
-    for (let i = 0; i < openCards.length; i++) {
-        openCards[i].parentNode.classList.remove("show");
-    }
-}
-openCards = [];
-initGame();
+
+
+// Event listener -> on click on deck, flip card
+deck.addEventListener('click', turnCard);
